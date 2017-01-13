c476965e49844123abe50df098aeafb209bfefae
Preslav Rachev <rachev.preslav@gmail.com>
2017-01-13 12:02:52 +0100

    First commit

---
+++ app/src/main/java/com/preslavrachev/mediumreader/TopPostsActivity.java
@@ -0,0 +1,13 @@
+package com.preslavrachev.mediumreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopPostsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_posts);
+    }
+}
